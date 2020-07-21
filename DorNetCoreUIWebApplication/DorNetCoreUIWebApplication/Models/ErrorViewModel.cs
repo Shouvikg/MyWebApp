81e6438df6a17db206a41826ffb852f134f4caac
Shouvik Gupta <10597353+Shouvikg@users.noreply.github.com>
2020-07-21 10:09:41 +0530

    .Net Core project added

---
+++ DorNetCoreUIWebApplication/DorNetCoreUIWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DorNetCoreUIWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
